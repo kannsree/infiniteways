be0dfdf933c1a3d7876543fccc3c486ad0b7481a
kannsree <39234920+kannsree@users.noreply.github.com>
2018-05-13 21:45:08 +0530

    Add files via upload

---
+++ resources/databases/infiniteways.php
@@ -0,0 +1,17 @@
+<?php
+	/*$servername = "localhost";
+	$username = "root";
+	$passworda = "";
+	$dbname = "infiniteways";
+
+	$servername = "sql108.epizy.com";
+	$username = "epiz_21121855";
+	$passworda = "Kannsree1";
+	$dbname = "epiz_21121855_infiniteways";*/
+	
+	$servername="localhost:3306";
+	$username="infinit6_ks";
+	$passworda="Kannsree@1";
+	$dbname="infinit6_inifiniteways";
+
+?>
